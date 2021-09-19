8ab69408f9689c548b615c75c56f3c7f17cc60b9
SudipKumarSah-0934 <bsse0934@iit.du.ac.bd>
2021-09-19 13:27:49 +0600

    First release

--- test_index.py
+++ test_index.py
@@ -2,6 +2,5 @@ from index import add
 
 def test_add():
     assert add(1,2) == 3
-    assert add(1, "asdf") == "Invalid Input"
-
+    #assert add(1, "asdf") == "Invalid Input"
     # assert add(1,"abc") == "Invalid Input"
